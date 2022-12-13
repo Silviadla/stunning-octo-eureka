5d0f76069613c29359d50d21e49f80a74891d87e
Silvia <silviadelucasalv@gmail.com>
2022-12-13 12:40:59 +0100

    Añadir lineas, modificar readme

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Silvia De Lucas");
+    System.out.println("13 de diciembre de 2022");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("León");
   }
   
 }
